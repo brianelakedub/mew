a20f920cbd6a0883a926d4ef1cb4df6f372edd91
brianelakedub <74145119+brianelakedub@users.noreply.github.com>
2020-11-09 00:14:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -37,6 +37,15 @@
 			height="100%"
 			scrolling="auto"> 
 </iframe> 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YQ7V4MYRQ1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YQ7V4MYRQ1');
+</script>
 
 </body> 
 
